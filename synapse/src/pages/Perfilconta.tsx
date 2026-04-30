66ed35eab2d2de8ef386283a7db5612ece857846
Libslibslibs <sophieadolfomazaro@gmail.com>
2026-04-30 08:19:00 -0300

    segundo commit

---
+++ synapse/src/pages/Perfilconta.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+import {useNavigate} from "react-router-dom"
+
+function Perfilconta(){
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <header> </header>
+        </div>
+    );
+}
+
+export default Perfilconta;
