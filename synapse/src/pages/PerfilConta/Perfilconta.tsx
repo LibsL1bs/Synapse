c2767bab874250f951f15a581833c10a2ff0761f
giovanna2dias <giovanna.d.p.dia@gmail.com>
2026-04-30 13:04:57 -0300

    gigi

--- synapse/src/pages/PerfilConta/Perfilconta.tsx
+++ synapse/src/pages/PerfilConta/Perfilconta.tsx
@@ -5,10 +5,23 @@ function Perfilconta(){
     const navigate = useNavigate();
 
     return (
-        <div>
-            <header> </header>
+        <>
+            <div>
+                <header className="bg-gray-900 text-white text-lg w-full p-4 h-20"> PERFIL </header>
+            </div>
+        <div className="bg-gray-900 text-white text-lg w-full p-4 h-56 border border-gray-500">
+            <h1 className="text-white text-lg">
+                Informações do Atleta
+            </h1>
+            <div className="flex gap-4 flex flex-col items-center">
+            <input className=" border" placeholder="Nome" />
+            <input className="border" placeholder="Email" />
+            </div>
         </div>
+            <div className="bg-gray-900 text-white text-lg w-full p-4 h56 border border-gray-500">
+        </>
     );
+
 }
 
 export default Perfilconta;
