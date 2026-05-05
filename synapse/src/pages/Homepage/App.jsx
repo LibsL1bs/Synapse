6367e115d79803b627ea3b3ed09fe42c0b43103a
giovanna2dias <giovanna.d.p.dia@gmail.com>
2026-05-05 14:25:24 -0300

    voltando

--- synapse/src/pages/Homepage/App.jsx
+++ synapse/src/pages/Homepage/App.jsx
@@ -4,8 +4,6 @@ function App() {
   return (
     <nav className="flex gap-4 p-4 bg-slate-900 text-slate-100">
       <Link to="/">Home</Link>
-      <h1 className="text-2xl font-bold">Synapse</h1>
-      <h1>aaaaaaaaaaaa</h1>
       <Link to="/login">Login (pendente)</Link>
       <Link to="/cadastro">Cadastro (pendente)</Link>
     </nav>
