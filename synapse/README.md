8bd198b5a3ce3983d54137c0ab86c460a939c30f
giovanna2dias <giovanna.d.p.dia@gmail.com>
2026-05-05 14:23:38 -0300

    testeaaaaaa

--- synapse/README.md
+++ synapse/README.md
@@ -30,4 +30,3 @@ If you are developing a production application, we recommend using TypeScript wi
 
 
 
-
--- synapse/src/pages/Homepage/App.jsx
+++ synapse/src/pages/Homepage/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <nav className="flex gap-4 p-4 bg-slate-900 text-slate-100">
       <Link to="/">Home</Link>
+      <h1 className="text-2xl font-bold">Synapse</h1>
       <Link to="/login">Login (pendente)</Link>
       <Link to="/cadastro">Cadastro (pendente)</Link>
     </nav>
