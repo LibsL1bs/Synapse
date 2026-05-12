64a3ba9f0a454fc141874ff55442f5e162dc07cf
Livynha <216908871+LivyOliv017@users.noreply.github.com>
2026-05-12 13:37:56 -0300

    Criar Conta (Funcionando

--- synapse/src/components/CriarConta/Inputs/ConfirmPass.jsx
+++ synapse/src/components/CriarConta/Inputs/ConfirmPass.jsx
@@ -1,4 +1,4 @@
-export default function ConfirmPass({ value, onChange }) {
+export default function ConfirmPass({ valor, mudar }) {
     return (
         <>
             <label className="text-slate-300 text-sm ">Confirmar Senha</label>
@@ -6,8 +6,8 @@ export default function ConfirmPass({ value, onChange }) {
             <input
                 type="password"
                 placeholder="Confirme sua senha"
-                value={value}
-                onChange={onChange}
+                value={valor}
+                onChange={mudar}
                 className="rounded-lg border border-slate-700 bg-slate-950 px-4 py-2.5 text-sm text-slate-100
         outline-none placeholder:text-slate-500
         focus:ring-2 focus:ring-cyan-500 transition"
